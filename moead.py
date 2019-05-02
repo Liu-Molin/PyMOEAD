dfc5937d6cb5c7e1769367e7d6d1f36c7fce6029
Molin Liu <molin@live.cn>
2019-05-02 18:14:26 +0800

    Fileloader and lambda initialization

--- moead.py
+++ moead.py
@@ -8,22 +8,99 @@
 # Copyright  © Rockface 2018 - 2019
 # --------------------------------------------------------------
 
-import sys
+import sys, os
+import numpy as np
+from decimal import *
+import utilis
 
-class asset():
-	def __init__(self, id, current_price, holding, cost_buy, cost_sell, vcost_buy, vcost_sell, min_buy, min_sell, max_buy, max_sell, mean_income, diviation_risk):
-		self.id = id
-		self.current_price = current_price
-		self.holding = holding
+
+DATA_DIR='/Users/meow/Desktop/DP/PyMoead/DataSet'
+DATA_PATH = os.path.join(DATA_DIR, 'portreb1.txt')
+
+class Constraint():
+	def __init__(self):
+		pass
+class Asset():
+	
+	def __init__(self, line):
+		line = line.split()
+		self.id = 0
+		self.current_price = 0
+		self.holding = 0
+		self.cost_sell = 0
+		self.cost_buy = 0
+		self.vcost_sell = 0.0
+		self.vcost_buy = 0.0
+		self.min_sell = 0
+		self.min_buy = 0
+		self.max_sell = 0
+		self.max_buy = 0
+		self.mean_income = 0.0
+		self.diviation_risk = 0.0
+		self.__set_data(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7], line[8], line[9])
+
+	def __str__(self):
+		info = '\nID:\t' + str(self.id) + '\nCurrent price:\t' + str(self.current_price) + '\nHolding:\t' + str(self.holding) + '\nMean income:\t' + str(self.mean_income) +'\nDiviation risk:\t' + str(self.diviation_risk)
+		return info
+	def __set_data(self, current_price, holding, cost_buy, cost_sell, vcost_buy, vcost_sell, min_buy, min_sell, max_buy, max_sell):
+		self.current_price = int(current_price)
+		self.holding = int(holding)
 		self.cost_sell = cost_sell
 		self.cost_buy = cost_buy
 		self.vcost_sell = vcost_sell
 		self.vcost_buy = vcost_buy
-		self.min_sell = min_sell
-		self.min_buy = min_buy
+		self.min_sell = int(min_sell)
+		self.min_buy = int(min_buy)
 		self.max_sell = max_sell
 		self.max_buy = max_buy
-		self.mean_income = mean_income
-		self.diviation_risk = diviation_risk
-
 	
+	def standarize(self):
+		self.holding += int(self.max_buy)
+
+class Lamb():
+	
+	
+	def __init__(self, lambid):
+		self.lambid = lambid
+		self.lamb = []
+		self.neighbor = []
+		pass
+	def __str__(self):
+		return str(self.lambid) + '\t' + str(self.lamb)
+
+class PyMOEAD():
+
+	def __init__(self, dataPath):
+		self.num_solution = 100
+		self.num_epoch = 100
+		self.num_neighbor = 5
+
+		self.dataPath = dataPath
+		pass
+
+	def __initLamb(self, num_lamb, num_neighbor):
+		lambList = []
+		for i in range(num_lamb):
+			tempLamb = Lamb(i)
+			
+			weight1 = float(i)/float(num_lamb)
+			weight2 = 1-weight1
+			tempLamb.lamb.append(weight1)
+			tempLamb.lamb.append(weight2)
+			lambList.append(tempLamb)
+		return lambList
+	def __param(self, num_solution, num_epoch, num_neighbor):
+		self.num_solution = num_solution
+		self.num_epoch = num_epoch
+		self.num_neighbor = num_neighbor
+
+	def __initPopulation(self, num_solution):
+		pass
+	def go(self, num_solution = 100,  num_epoch = 100, num_neighbor = 5):
+		self.__param(num_solution, num_epoch, num_neighbor)
+		self.problem, self.AssetList = utilis.file_loader(self.dataPath)
+		self.__initLamb(self.num_solution, self.num_neighbor)
+
+if __name__ == "__main__":
+	mop = PyMOEAD(DATA_PATH)
+	mop.go()
--- utilis.py
+++ utilis.py
@@ -8,7 +8,38 @@
 # Copyright  © Rockface 2018 - 2019
 # --------------------------------------------------------------
 
-import sys
+import sys, os
 import fileinput
+import moead
 
-DATA_DIR='/Users/meow/Desktop/DP/PyMoead/DataSet'
+
+
+def file_loader(dataPath):
+	num_line = 0
+	problem = moead.Constraint()
+	AssetList = []
+	for line in fileinput.input(dataPath):
+		if num_line==0:
+			line = line.split()
+			problem.num_assets = int(line[0])
+			problem.num_port = int(line[1])
+		else:
+			if num_line<=problem.num_assets :
+				asset_cache = moead.Asset(line)
+				AssetList.append(asset_cache)
+			else:
+				if num_line<=(2*problem.num_assets):
+					tran = num_line-problem.num_assets-1
+					line = line.split()
+					AssetList[tran].mean_income = line[0]
+					AssetList[tran].diviation_risk = line[1]
+		num_line+=1
+	fund = 0
+	for i in range(len(AssetList)):
+		AssetList[i].id = i
+		fund+=AssetList[i].holding*AssetList[i].current_price
+		problem.fund = fund
+		AssetList[i].standarize()
+		##print(AssetList[i])
+	return problem, AssetList
+	
